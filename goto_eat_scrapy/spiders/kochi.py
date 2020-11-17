607b04f9b834f54aa5a8806321ff064d0ccf12d0
KOIZUMI teruaki <1755900+terukizm@users.noreply.github.com>
2020-11-18 00:39:36 +0900

    add: 高知県

---
+++ goto_eat_scrapy/spiders/kochi.py
@@ -0,0 +1,36 @@
+
+import scrapy
+import json
+from goto_eat_scrapy.items import ShopItem
+
+class KochiSpider(scrapy.Spider):
+    """
+    usage:
+      $ scrapy crawl kochi -O 39_kochi.csv
+    """
+    name = 'kochi'
+    allowed_domains = [ 'gotoeat-kochi.jp' ]
+
+    start_urls = [
+        'https://www.gotoeat-kochi.com/js/shop_list.php',   # jsonが帰ってくる
+    ]
+
+    def parse(self, response):
+        # json形式なので、response.body(bytes)を直接読める
+        for row in json.loads(response.body):
+            # おそらく
+            #   0: area_code,
+            #   1: area_name,
+            #   2: genre_code,
+            #   3: genre_name,
+            #   4: ???,
+            #   5: shop_name,
+            #   6: shop_name_kana,
+            #   7: address,
+            #   8: tel
+            yield ShopItem(
+                genre_name = row[3],
+                shop_name = row[5],
+                address = row[7],
+                tel = row[8],
+            )
