9446c751651c36048adb11b93d79d01818761b48
KOIZUMI teruaki <1755900+terukizm@users.noreply.github.com>
2020-11-28 22:58:37 +0900

    add: 宮城県
    公式サイトのGoogle Mapのポイントがここ最近プロットされなくなっていたので補完用として追加

---
+++ goto_eat_scrapy/spiders/miyagi.py
@@ -0,0 +1,44 @@
+import re
+import scrapy
+from goto_eat_scrapy.items import ShopItem
+from goto_eat_scrapy.spiders.abstract import AbstractSpider
+
+class MiyagiSpider(AbstractSpider):
+    """
+    usage:
+      $ scrapy crawl miyagi -O miyagi.csv
+    """
+    name = 'miyagi'
+    allowed_domains = [ 'gte-miyagi.jp' ]
+
+    # @see https://gte-miyagi.jp/available.html
+    # 宮城県はページングなし
+    start_urls = [
+        'https://gte-miyagi.jp/available_aobaku.php',
+        'https://gte-miyagi.jp/available_miyaginoku.php',
+        'https://gte-miyagi.jp/available_wakabayashiku.php',
+        'https://gte-miyagi.jp/available_taihakuku.php',
+        'https://gte-miyagi.jp/available_izumiku.php',
+        'https://gte-miyagi.jp/available03.php',
+        'https://gte-miyagi.jp/available04.php',
+    ]
+
+    def parse(self, response):
+        # 各加盟店情報を抽出
+        self.logzero_logger.info(f'💾 url = {response.request.url}')
+        for article in response.xpath('//div[@class="SLCont"]//dl[@class="shopList"]'):
+            item = ShopItem()
+            item['shop_name'] = article.xpath('.//dt/text()').get().strip()
+            item['genre_name'] = article.xpath('.//dd[1]/span[2]/text()').get().strip()
+
+            place = ' '.join(article.xpath('.//dd[2]/span[2]/text()').getall())
+            m = re.match(r'〒(?P<zip_code>.*?)\s(?P<address>.*)', place)
+            item['address'] = m.group('address').strip()
+            item['zip_code'] = m.group('zip_code').strip()
+            item['tel'] = article.xpath('.//dd[3]/span[2]/text()').get().strip()
+
+            # MEMO: 本来は@hrefで取りたいが、aリンクが貼られてないのもあるため(2020/11/28)
+            item['offical_page'] = article.xpath('.//dd[4]/span[@class="url"]/text()').get()
+
+            self.logzero_logger.debug(item)
+            yield item
