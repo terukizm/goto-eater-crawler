9ae6ae47ba9b16b00b78e858a72cc5be30a9e0f7
KOIZUMI teruaki <1755900+terukizm@users.noreply.github.com>
2020-12-29 14:07:26 +0900

    fix: 鹿児島のhtml構造に変更があったので追従、コメント修正

--- goto_eat_scrapy/spiders/kagoshima.py
+++ goto_eat_scrapy/spiders/kagoshima.py
@@ -12,10 +12,11 @@ class KagoshimaSpider(AbstractSpider):
     allowed_domains = [ 'kagoshima-cci.or.jp' ]
     start_urls = ['http://www.kagoshima-cci.or.jp/?p=20375']
 
-    # FIXME: 鹿児島は2020/12/04？にpdfをやめてhtmlにしてくれたが、おそらく元のExcelから行を非表示にしたものを
-    # ExcelのWebページ発行ウィザードとやらで出力しているだけなので、HTMLソースを見ると重複した内容が含まれている。
-    # 件数から見ておそらく「鹿児島市全域」と「それ以外」で２つのファイルがあり、それを出し分けしてると思うので、
+    # FIXME: 鹿児島は2020/12/04？にpdfをやめてhtmlにしてくれたが、おそらく元データのExcelからフィルタで行を非表示にしたものを
+    # ExcelのWebページ発行ウィザードで出力しているだけなので、HTMLソースを見ると重複した内容が含まれている。
+    # 件数から見ておそらく「鹿児島市全域」と「その他地域」で２つのファイルがあり、それを出し分けしてると思うので、
     # そんな感じでやっていってる(kcciさんの苦労が偲ばれる…)
+
     area_list = [
         '鹿児島市全域',
         # '〇薩摩川内市',
@@ -57,7 +58,7 @@ class KagoshimaSpider(AbstractSpider):
                 url = p.xpath('.//a/@href').get().strip()
                 yield scrapy.Request(url, callback=self.parse_from_area_html, meta={'area_name': area_name})
             else:
-                # MEMO: 暫定的にExcelのWebページ発行ウィザードの仕様に合わせてコメントアウト、最後まで本気でこれで行きそうな感じなら実装を直すが、
+                # MEMO: 暫定的にExcelのWebページ発行ウィザードの仕様に合わせてコメントアウト。最後まで本気でこれで行きそうだったら実装を直すが、
                 # 鹿児島はもう3回くらい出力形式が変わっているので、もう諦めてやっつけ仕事でいく...
                 # self.logzero_logger.warning(f'鹿児島商工会議所エラー: 「{area_name}」 is not found.')
                 pass
@@ -65,18 +66,16 @@ class KagoshimaSpider(AbstractSpider):
     def parse_from_area_html(self, response):
         area_name = response.meta['area_name']
         for article in response.xpath('//table/tr'):
-            if article.xpath('.//td[2]/a[contains(text(), "検索")]').get():
+            if article.xpath('.//td[2]/a/span[contains(text(), "検索")]').get():
                 item = ShopItem()
                 # MEMO: 店舗名、住所に改行が入ってるものがある(item pipelineで対応)
                 item['shop_name'] = article.xpath('.//td[3]/text()').get().strip()
                 item['address'] = article.xpath('./td[4]/text()').get().strip()
-
-                # MEMO: エリア名はhtmlから取れなくもないが、Excelベースの表構造なのでしんどい
-                # area_list経由で取るのも、実質2データしかなく、それらをdisplay:noneで出し分けしている
-                # (おそらく一つのExcelシートを非表示にしたりして出してる)ので大変めんどい
-                # 最後まで本気でこのWebページ形式で行く感じだったら、諦めた対応するかも
-
                 # item['genre_name'] = None   # 鹿児島はジャンル情報なし
 
+                # MEMO: エリア名はhtmlから取れなくもないが、Excelベースの表構造になっているので相当しんどい
+                # (実質2データしかなく、それらをdisplay:noneで出し分けしており、
+                # おそらく一つのExcelシートにフィルタをかけて行を非表示にしたりして出し分けしている)
+                # 最後まで本気でこのWebページ形式で行く感じだったら、諦めて対応する…かも
 
                 yield item
