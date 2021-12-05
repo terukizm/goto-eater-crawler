47616815ea4d5ff5ffd191d96e785e66b18b4af3
KOIZUMI teruaki <1755900+terukizm@users.noreply.github.com>
2021-12-06 00:52:29 +0900

    hotfix: 愛媛県も11末でキャンペーン終了

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ class Main:
                 "niigata", # 2021/10/31をもってキャンペーン終了
                 "yamaguchi", # 2021/10/31をもってキャンペーン終了
                 "toyama", # 2021/11/30をもってキャンペーン終了
+                "ehime", # 2021/11/30をもってキャンペーン終了
             ]
             process = CrawlerProcess(self.settings)
             targets = [x for x in process.spider_loader.list() if not x in ignores]
